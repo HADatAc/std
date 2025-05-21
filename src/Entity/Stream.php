9c5a0f79777e567dee99503dfbc961196f059f53
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-05-21 13:29:08 +0100

    New column Stream on DA files

--- src/Entity/Stream.php
+++ src/Entity/Stream.php
@@ -12,7 +12,7 @@ class Stream {
     return $header = [
       'element_uri' => t('URI'),
       'element_name' => t('Name'),
-      'element_pattern' => t('Datafile Pattern'),
+      'element_pattern' => t('Datafile Pattern / Protocol -(IP:Port)'),
       'element_semanticdatadictionary' => t('Semantic Data Dictionary'),
       'element_deployment' => t('Deployment'),
     ];
@@ -42,15 +42,18 @@ class Stream {
         $deployment = $element->deployment->label;
       }
       $pattern = ' ';
-      if ($element->datasetPattern != NULL) {
+      if ($element->method === 'files') {
+        // 'element_stream' => t('<span style="display: inline-block; max-width: 30ch; white-space: normal; overflow-wrap: anywhere; word-break: break-all;">' . (isset($stream) ? ($stream->method === 'file' ? $stream->datasetPattern : ($stream->messageProtocol . ' (' . $stream->messageIP .":".$stream->messagePort)) : '-') . '</span>'),
         $pattern = $element->datasetPattern;
+      } else {
+        $pattern = ($stream->messageProtocol . ' (' . $stream->messageIP .":".$stream->messagePort) . ")";
       }
       $root_url = \Drupal::request()->getBaseUrl();
       $encodedUri = rawurlencode(rawurlencode($element->uri));
       $output[$element->uri] = [
         'element_uri' => t('<a href="'.$root_url.REPGUI::DESCRIBE_PAGE.base64_encode($uri).'">'.$uri.'</a>'),  
         'element_name' => t($element->label),   
-        'element_pattern' => t($pattern),     
+        'element_pattern' => t('<span style="display: inline-block; max-width: 80ch; white-space: normal; overflow-wrap: anywhere; word-break: break-all;">' . $pattern . '</span>'),     
         'element_semanticdatadictionary' => t($semanticDataDictionary),     
         'element_deployment' => t($deployment),     
       ];
