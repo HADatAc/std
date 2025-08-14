97f52b8081b64b1f02d02ee06ce8049459497107
Nuno Simões <105175988+numibesi@users.noreply.github.com>
2025-08-14 16:31:40 +0100

    .

--- src/Form/ManageStudyForm.php
+++ src/Form/ManageStudyForm.php
@@ -197,7 +197,7 @@ class ManageStudyForm extends FormBase
     // $form['#attached']['library'][] = 'core/drupal.autocomplete';
     $form['#attached']['library'][] = 'rep/pdfjs';
     $form['#attached']['library'][] = 'rep/webdoc_modal';
-    $base_url = \Drupal::request()->getSchemeAndHttpHost() . \Drupal::request()->getBaseUrl();
+    $base_url = (\Drupal::request()->headers->get('x-forwarded-proto') === 'https' ? 'https://':'http://'). \Drupal::request()->getHost() . \Drupal::request()->getBaseUrl();
     $form['#attached']['drupalSettings']['webdoc_modal'] = [
       'baseUrl' => $base_url,
     ];
